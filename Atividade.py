5498f15b1775f3691a8a7caeac626ce1635ff170
Marcos Vinícius <98711516+vrsmarcos26@users.noreply.github.com>
2024-03-11 10:40:17 -0300

    Update Atividade.py

--- Atividade.py
+++ Atividade.py
@@ -1,16 +1,64 @@
-'''Criar lista'''
+#empacotamento e desempacotamento de parametros
 
-lista = [1,2,3,4,5]
-print(lista)
+def soma (*valor): #funções -----
+    r = 0
+    for i in valor:
+        r += i
+    return r
 
-"""Reverter"""
+def exibeforma(a,b,c):
+    print("1. valor = {} ".format(a))
+    print("2. valor = {} ".format(b))
+    print("3. valor = {} ".format(c))
 
-lista.reverse()
-print(lista)
+def returnMult (x,y):
+    ad = x + y
+    su = x - y
+    mu = x * y
+    dv = x / y
+    return(ad,su,mu,dv)
 
-pdr = 1
+#----- FATORIAL -----
 
-for num in lista:
-    pdr*=num
+def fatorial (n):
+    if n <= 1:
+        return 1
+    else:
+        return n * fatorial(n-1)
 
-print(pdr)
+def somaLista(L): # ------= soma a lista de forma recursiva------
+    print (L)
+    if L ==[]:
+        return 0
+    else:
+        return L[0] + somaLista(L[1:])
+
+#DocString 
+x = 10
+print()
+
+def escopo():
+    y = x * 2
+    print("x global existe fora da funcao {}")
+
+
+#soma ----
+
+print(soma(3,9))
+print(soma(1,2,3,4))
+
+#--- empacotamento ---
+L = [31,77,193]
+exibeforma(*L)
+
+
+
+#------0 retorna multiplos valores -------
+a = 4
+b = 6
+s = returnMult(a,b)
+print("Resultados {}".format(s))
+
+print(fatorial(5))
+l = [1,2,3,4,5,6,7,8,9]
+print(somaLista(l))
